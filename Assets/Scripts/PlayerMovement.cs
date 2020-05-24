e8e6cf6f57cd07ad46fdfcc0e3c3dab378cf9f5f
Jojen <jojen@JojendeMacBook-Pro.local>
2020-05-24 13:23:19 +0800

    Update model

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float speed = 2f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called when collapse or interaction
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // 0f denote the float type
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if(!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        if (m_Rigidbody.name == "Model_Professor")
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                // If pressing any of the direction key, go ahead
+                transform.Translate(speed * Vector3.forward * Time.deltaTime);
+            }
+                
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+            m_Rigidbody.MoveRotation(m_Rotation);
+        }
+        else if (m_Rigidbody.name == "JohnLemon")
+        {
+            
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+            m_Rigidbody.MoveRotation(m_Rotation);
+        }
+    }
+}
