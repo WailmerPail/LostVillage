c12ff1c5a898741f79598d9a5722a92a83cb44c1
LI <psw-end@qq.com>
2020-05-26 23:13:56 +0800

    添加素材

---
+++ Assets/Scripts/DialogMsg.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogMsg : MonoBehaviour
+{
+    public Image headImage;
+    public GameObject nameText;
+    public GameObject messageText;
+    // Start is called before the first frame update
+    public static int maxshowFrame = 1000;
+    public int leftshowFrame = 1000;
+    private bool visible = false;
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+        visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!visible)
+        {
+            return;
+        }
+        leftshowFrame--;
+        if (leftshowFrame == 0)
+        {
+            leftshowFrame = maxshowFrame;
+            gameObject.SetActive(false);
+            visible = false;
+        }
+    }
+
+    public void SetReminder(string name, string message, string image)
+    {
+        gameObject.SetActive(true);
+        visible = true;
+        nameText.GetComponentInChildren<Text>().text = name;
+        messageText.GetComponentInChildren<Text>().text = message;
+        headImage.sprite = Resources.Load<Sprite>(image);
+    }
+
+}
---
+++ Assets/Scripts/DialogTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTestScript : MonoBehaviour
+{
+    public DialogMsg msgCaller = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            msgCaller.SetReminder("VINA", "aaafsdfsdfsfsdfsdfs msg", "VINA_happy");
+        }
+    }
+}
