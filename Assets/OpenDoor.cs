ea5240a1b3beae1c8c2c3a3ab8273b4e52158256
Jojen <jojen@JojendeMacBook-Pro.local>
2020-05-26 09:20:11 +0800

    Update door control

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool isKeyAExisted;
+
+    public bool isPlayerEnter;
+
+    private bool _isOpen;
+
+    private bool _openDoor;
+    public float degree;
+
+    public Transform anchor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isOpen = false;
+        _openDoor = false;
+        isPlayerEnter = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isKeyAExisted && isPlayerEnter && !_isOpen && !_openDoor)
+        {
+            Debug.Log("Opening Door");
+            _isOpen = true;
+        }
+        if (_isOpen && transform.rotation.y > degree)
+        {
+            transform.RotateAround(anchor.position, Vector3.down, 3f);
+        }
+    }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     Debug.Log("Player enter region");
+    // }
+}
---
+++ Assets/Scripts/DoorObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorObserver : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            Debug.Log("Player enter");
+            gameObject.GetComponentInParent<OpenDoor>().isPlayerEnter = true;
+        }
+        
+    }
+}
