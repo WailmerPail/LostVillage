6f37a8e0c41a1fc944850f676acae0cd7bea6e1d
LI <psw-end@qq.com>
2020-05-27 18:11:26 +0800

    增加按f拾取钥匙

--- Assets/Scripts/ItemObserver.cs
+++ Assets/Scripts/ItemObserver.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ItemObserver : MonoBehaviour
 {
+    public GameObject root;
     public Transform player;
     public string message;
     public string avatar;
     public string Audio;
     private bool m_IsPlayerInRange;
     private bool flag_onlytriggerOnce = true;
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -39,6 +41,12 @@ public class ItemObserver : MonoBehaviour
         {
             GameObject.Find("DialogCanvas").GetComponent<DialogMsg>().SetReminder("VINA", message, avatar, Audio);
             flag_onlytriggerOnce = false;
+        }
+        if (Input.GetKeyDown(KeyCode.F)) {
+            if (m_IsPlayerInRange) {
+                print("I am picked!");
+                root.SetActive(false);
+            }
         }
     }
 
